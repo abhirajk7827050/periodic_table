997dee05fd9e878cab91f690b395c2839eda4085
Abhiraj <abhirajk7827050@gmail.com>
2026-04-10 19:18:48 -0400

    feat: add element lookup logic

--- element.sh
+++ element.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
-
+# Added logic for atomic numbers
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
